1b3feaceb06ab288545cc8b9ccfee5e3df0dbfc0
tikijian <timurt1988@gmail.com>
2016-01-10 19:35:57 +0300

    Initial Commit

---
+++ Chip8/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chip8
+//
+//  Created by Тимур on 10.01.16.
+//  Copyright © 2016 Tikijian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
